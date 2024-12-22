8239f653878354e16258effd0dea1cf06ba4b976
aswanthmc <mcaswanth5@gmail.com>
2024-12-22 10:03:54 +0530

    test the connection of databse

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const pool =require('../db');
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.render('home');
+router.get('/', async function(req, res) {
+  try {
+    const{ rows }=await pool.query('select name,designation from autho');
+    res.render('home',{data:rows});
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('error fectching')
+  }
 });
 
 module.exports = router;
