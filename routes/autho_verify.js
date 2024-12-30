7860076535f32580d13a672febc00bd4baf340db
aswanthmc <mcaswanth5@gmail.com>
2024-12-30 20:42:09 +0530

    adding the authority verify router and also try to include photo view but not get

---
+++ routes/autho_verify.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+const pool =require('../db')
+
+router.get('/', async function(req, res) {
+  try {
+    const query = `SELECT name, email, phone_number, office_address, designation, department, employee_id, id_proof, photo FROM authority`;
+    const { rows } = await pool.query(query);
+
+    res.render('autho_verify', { data: rows });
+  } catch (error) {
+    console.error('Error fetching sorted data:', error.message);
+    res.status(500).send('An error occurred while fetching data.');
+  }
+  });
+  
+
+
+  module.exports = router;
