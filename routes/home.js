12157d9b2dfb1d84a164aa97e99463ddf0d067fd
aswanthmc <mcaswanth5@gmail.com>
2024-12-30 20:43:49 +0530

    adding the authority ul list and connect the data base

--- routes/home.js
+++ routes/home.js
@@ -4,13 +4,24 @@ const pool =require('../db');
 
 /* GET users listing. */
 router.get('/', async function(req, res) {
+  const { sortBy = 'district', order = 'ASC' } = req.query;
+
+
+  const validColumns = ['name', 'designation', 'district'];
+  const validOrder = ['ASC', 'DESC'];
+
+  if (!validColumns.includes(sortBy) || !validOrder.includes(order)) {
+    return res.status(400).send('Invalid sorting parameters.');
+  }
+
   try {
-    const{ rows }=await pool.query('select name,designation from autho');
-    res.render('home',{data:rows});
+    const query = `SELECT name, designation, district FROM autho ORDER BY ${sortBy} ${order}`;
+    const { rows } = await pool.query(query);
+
+    res.render('home', { data: rows });
   } catch (error) {
-    console.error(error);
-    res.status(500).send('error fectching')
+    console.error('Error fetching sorted data:', error.message);
+    res.status(500).send('An error occurred while fetching data.');
   }
 });
-
 module.exports = router;
