8d86061a6e48f56f4d7443c1e6e3bc3fc5ed7fea
aswanthmc <mcaswanth5@gmail.com>
2025-01-06 09:41:29 +0530

    trying to add approving but not get and clear the code

--- routes/autho_verify.js
+++ routes/autho_verify.js
@@ -4,7 +4,7 @@ const pool =require('../db')
 
 router.get('/', async function(req, res) {
   try {
-    const query = `SELECT name, email, phone_number, office_address, designation, department, employee_id, id_proof, photo FROM authority`;
+    const query = `SELECT id,name, email, phone_number, office_address, designation, department, employee_id, id_proof, photo FROM authority`;
     const { rows } = await pool.query(query);
 
     res.render('autho_verify', { data: rows });
@@ -13,26 +13,5 @@ router.get('/', async function(req, res) {
     res.status(500).send('An error occurred while fetching data.');
   }
   });
-  router.post('/verify/approve/:id', async (req, res) => {
-    const id = req.params.id; 
   
-    try {
-      
-      const checkQuery = 'SELECT * FROM authority WHERE id = $1';
-      const checkResult = await pool.query(checkQuery, [id]);
-  
-      if (checkResult.rowCount === 0) {
-        return res.status(404).send('Authority record not found.');
-      }
-  
-      
-      const updateQuery = 'UPDATE authority SET verified = true WHERE id = $1';
-      await pool.query(updateQuery, [employee_id]);
-  
-      res.status(200).send('Authority approved successfully.');
-    } catch (error) {
-      console.error('Error approving authority:', error.message);
-      res.status(500).send('An error occurred while approving authority.');
-    }
-  });
   module.exports = router;
